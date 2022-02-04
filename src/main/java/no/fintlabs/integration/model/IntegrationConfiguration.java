98fd7b15738cd3ab3fc2f344c43faab9d466af3b
ingrie <ingride89@gmail.com>
2022-02-04 12:36:33 +0100

    FST-124 add selected form to the intconf model

--- src/main/java/no/fintlabs/integration/model/IntegrationConfiguration.java
+++ src/main/java/no/fintlabs/integration/model/IntegrationConfiguration.java
@@ -19,6 +19,7 @@ public class IntegrationConfiguration {
     private String id;
     private String name;
     private String description;
+    private String selectedForm;
     private int version;
 
     private RecordConfiguration recordConfiguration;
