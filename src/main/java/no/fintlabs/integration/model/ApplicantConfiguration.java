24c7294b818efe8ee20b12d07b7b83c72a4af149
ingrie <ingride89@gmail.com>
2022-03-07 10:52:24 +0100

    FST-124 change ssn to nin

--- src/main/java/no/fintlabs/integration/model/ApplicantConfiguration.java
+++ src/main/java/no/fintlabs/integration/model/ApplicantConfiguration.java
@@ -8,6 +8,6 @@ import java.util.List;
 @Data
 public class ApplicantConfiguration {
     private String organisationNumber;
-    private String socialSecurityNumber;
+    private String nationalIdentityNumber;
     private List<Field> fields = new ArrayList<>();
 }
