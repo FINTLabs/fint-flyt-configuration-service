c0e4428bd853ddb0c5a16f34a99140b0f805e597
ingrie <ingride89@gmail.com>
2022-03-08 15:04:49 +0100

    add "api/" to sakcontroller

--- src/main/java/no/fintlabs/integration/sak/SakController.java
+++ src/main/java/no/fintlabs/integration/sak/SakController.java
@@ -11,7 +11,7 @@ import org.springframework.web.server.ResponseStatusException;
 
 @Slf4j
 @RestController
-@RequestMapping("sak")
+@RequestMapping("api/sak")
 public class SakController {
 
     private final SakRequestService sakRequestService;
