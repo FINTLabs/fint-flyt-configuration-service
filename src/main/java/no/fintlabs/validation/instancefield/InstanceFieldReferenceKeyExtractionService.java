08b1aff3638c840a70bdd3588193684531a7e0a8
Eivind Morch <eivind@morch.priv.no>
2022-10-28 11:37:44 +0200

    Handle null values

--- src/main/java/no/fintlabs/validation/instancefield/InstanceFieldReferenceKeyExtractionService.java
+++ src/main/java/no/fintlabs/validation/instancefield/InstanceFieldReferenceKeyExtractionService.java
@@ -3,6 +3,7 @@ package no.fintlabs.validation.instancefield;
 import org.springframework.stereotype.Service;
 
 import java.util.Collection;
+import java.util.Collections;
 import java.util.regex.MatchResult;
 import java.util.regex.Pattern;
 
@@ -12,7 +13,9 @@ public class InstanceFieldReferenceKeyExtractionService {
     private static final Pattern ifReferenceExtractionPattern = Pattern.compile("\\$if\\{[^}]+}");
 
     public Collection<String> extractIfReferenceKeys(String value) {
-        return ifReferenceExtractionPattern.matcher(value)
+        return value == null
+                ? Collections.emptyList()
+                : ifReferenceExtractionPattern.matcher(value)
                 .results()
                 .map(MatchResult::group)
                 .map(ifReference -> ifReference.replace("$if{", "").replace("}", ""))
