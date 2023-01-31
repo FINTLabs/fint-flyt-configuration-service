61267c7059653b471e1419dae4eb49d852554e27
Eivind Morch <eivind@morch.priv.no>
2023-01-31 14:05:16 +0100

    FF-477 #comment Add valid validation rule

--- src/main/java/no/fintlabs/model/configuration/dtos/ElementsFromCollectionMappingDto.java
+++ src/main/java/no/fintlabs/model/configuration/dtos/ElementsFromCollectionMappingDto.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
@@ -20,6 +21,7 @@ public class ElementsFromCollectionMappingDto {
     @NotEmpty
     private List<@NotBlank String> instanceCollectionReferencesOrdered = new ArrayList<>();
 
+    @Valid
     @NotNull
     private ElementMappingDto elementMapping;
 
