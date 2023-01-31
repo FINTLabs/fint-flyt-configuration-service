3007f8150d10a38d8767592986f21a6bb53b3c14
Eivind Morch <eivind@morch.priv.no>
2023-01-31 13:36:08 +0100

    FF-477 #comment Add notnull validation rule

--- src/main/java/no/fintlabs/model/configuration/dtos/ElementsFromCollectionMappingDto.java
+++ src/main/java/no/fintlabs/model/configuration/dtos/ElementsFromCollectionMappingDto.java
@@ -6,6 +6,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
@@ -16,6 +17,7 @@ import java.util.List;
 @NoArgsConstructor
 public class ElementsFromCollectionMappingDto {
 
+    @NotEmpty
     private List<@NotBlank String> instanceCollectionReferencesOrdered = new ArrayList<>();
 
     @NotNull
