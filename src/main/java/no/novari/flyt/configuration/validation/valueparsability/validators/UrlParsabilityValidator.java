af7a7e189e40c06ccd4ffbb3b5664bb865221d0a
Egil Ballestad <egil.ballestad@vigoiks.no>
2025-11-26 15:47:56 +0100

    refactor

--- src/main/java/no/novari/flyt/configuration/validation/valueparsability/validators/UrlParsabilityValidator.java
+++ src/main/java/no/novari/flyt/configuration/validation/valueparsability/validators/UrlParsabilityValidator.java
@@ -4,6 +4,7 @@ import no.novari.flyt.configuration.model.configuration.entities.ValueMapping;
 import no.novari.flyt.configuration.validation.valueparsability.ValueParsabilityValidator;
 import org.springframework.stereotype.Service;
 
+import java.net.URI;
 import java.net.URL;
 
 @Service
@@ -17,7 +18,8 @@ public class UrlParsabilityValidator implements ValueParsabilityValidator {
     @Override
     public boolean isValid(String value) {
         try {
-            new URL(value).toURI();
+            URI uri = URI.create(value);
+            URL ignored = uri.toURL();
             return true;
         } catch (Exception e) {
             return false;
