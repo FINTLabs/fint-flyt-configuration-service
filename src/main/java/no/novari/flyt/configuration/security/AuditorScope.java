c024da781217064f6f9470bb1c411ac9b245a27f
Egil Ballestad <egil.ballestad@vigoiks.no>
2026-03-10 14:12:13 +0100

    moderize app by converting to kotlin, java 25 and replacing webflux with web

--- src/main/java/no/novari/flyt/configuration/security/AuditorScope.java
+++
@@ -1,7 +0,0 @@
-package no.novari.flyt.configuration.security;
-
-@FunctionalInterface
-public interface AuditorScope extends AutoCloseable {
-    @Override
-    void close();
-}
--- src/main/java/no/novari/flyt/configuration/validation/groups/ValueParsability.java
+++
@@ -1,4 +0,0 @@
-package no.novari.flyt.configuration.validation.groups;
-
-public interface ValueParsability {
-}
